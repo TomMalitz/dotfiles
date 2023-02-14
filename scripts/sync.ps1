e7470a45e1d6650d9b1db0c333df3ae0455e3ac6
TomMalitz <tmalitz214@gmail.com>
2023-02-14 01:41:24 -0600

    updating sync script for powershell config

--- scripts/sync.ps1
+++ scripts/sync.ps1
@@ -5,6 +5,12 @@ $greenCheck = @{
 
 Copy-Item "../nvim/" -Destination "~/AppData/Local/" -Recurse -Force
 Copy-Item "../powershell/" -Destination "~/.config/" -Recurse -Force
+if($isWindows) {
+  Copy-Item "../powershell/Microsoft.PowerShell_profile.ps1" -Destination "~/Documents/PowerShell/" -Force
+}
+# else {
+#   Copy-Item "../powershell/" -Destination "~/.config/" -Recurse -Force
+# }
 
 Write-Host "config files synced " -NoNewLine
 Write-Host @greenCheck
