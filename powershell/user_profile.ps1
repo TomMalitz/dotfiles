8ba061f9da79dbd115bd386810db38e98aec7536
TomMalitz <tmalitz214@gmail.com>
2023-02-14 01:41:11 -0600

    updating powershell posh

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -4,9 +4,10 @@ Set-Alias ll dir
 Set-Alias g git
 
 # Oh My Posh Config
-# oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/robbyrussel.omp.json' | Invoke-Expression
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/amro.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config '../powershell/posh.json' | Invoke-Expression
 
 # PowerShell Modules
 Import-Module Terminal-Icons
 Import-Module PSFzf
+
+Write-Host "Here"
