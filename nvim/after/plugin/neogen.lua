cb9a846316ca8171574ef9eaf84e054c823d2736
TomMalitz <tmalitz214@gmail.com>
2023-02-14 01:03:13 -0600

    removing after config files

--- nvim/after/plugin/neogen.lua
+++
@@ -1,5 +0,0 @@
-require('neogen').setup()
-
--- setup keymap for annotation generation
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<Leader>dg", ":lua require('neogen').generate()<CR>", opts)
--- nvim/after/plugin/treesitter.lua
+++
@@ -1,6 +0,0 @@
--- Treesitter setup
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "lua", "vim", "vue", "typescript", "tsx", "javascript", "java", "css"},
-  highlight = { enable = true },
-  indent = { enable = true }
-}
