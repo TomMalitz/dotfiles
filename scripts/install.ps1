3d4dfe5fa14b03b6d71e8d6d6495505bd5f0a42f
TomMalitz <tmalitz214@gmail.com>
2023-02-18 01:57:07 -0600

    removing scoop install comments

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -7,14 +7,6 @@ $greenCheck = @{
 Write-Host "installing dependencies..."
 if($isWindows) {
   Write-Host "OS detected: Windows"
-  # Write-Host "Installing Scoop"
-  # Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
-  # try {
-  #   irm get.scoop.sh | iex
-  # } catch {
-  #   Write-Host "Here"
-  #   Write-Output "Issue: $PSItem"
-  # }
   scoop bucket add extras
   scoop install neovim
   scoop install ripgrep
