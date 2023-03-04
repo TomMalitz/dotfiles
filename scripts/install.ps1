7413b291b548c896e02777152b7e2424eb611824
TomMalitz <tmalitz214@gmail.com>
2023-03-04 17:25:13 -0600

    adding update option to install script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -4,29 +4,40 @@ $greenCheck = @{
   # NoNewLine = $true
 }
 
+# install dependencies by default
+# pass -update or -u to upgrade dependencies
+$isUpdate = ($args[0] -eq "-update") -or ($args[0] -eq "-u")
+$scoopAction = "install"
+$brewAction = "install"
+if($isUpdate) { 
+  $scoopAction = "update" 
+  $brewAction = "upgrade" 
+}
+
 Write-Host "installing dependencies..."
 if($isWindows) {
   Write-Host "OS detected: Windows"
   scoop bucket add extras
-  scoop install wezterm
-  scoop install neovim
-  scoop install ripgrep
-  scoop install lazygit
-  scoop install starship
-  # scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
+  scoop $scoopAction wezterm
+  scoop $scoopAction neovim
+  scoop $scoopAction ripgrep
+  scoop $scoopAction lazygit
+  scoop $scoopAction starship
+  # scoop $scoopAction https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
 }
 elseif($isMacOS) {
   Write-Host "OS detected: macOS"
   brew tap wez/wezterm
-  brew install --cask wez/wezterm/wezterm
-  brew install neovim
-  brew install ripgrep
-  brew install lazygit
-  brew install starship
-  # brew install jandedobbeleer/oh-my-posh/oh-my-posh
+  brew $brewAction --cask wez/wezterm/wezterm
+  brew $brewAction neovim
+  brew $brewAction ripgrep
+  brew $brewAction lazygit
+  brew $brewAction starship
+  # brew $brewAction jandedobbeleer/oh-my-posh/oh-my-posh
 }
 elseif($isLinux) {
   Write-Host "OS detected: Linux"
+  Write-Host "This is not currently handled!"
 }
 
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
