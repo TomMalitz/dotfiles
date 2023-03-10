402db40c80af6f59107f156a668c3931377cfbd1
Thomas Malitz <tmalitz@cccis.com>
2023-03-10 12:36:25 -0600

    adding autotag closer

--- nvim/lua/plugins/text-editing.lua
+++ nvim/lua/plugins/text-editing.lua
@@ -1,6 +1,7 @@
 
 return {
   {'windwp/nvim-autopairs', config = true},
+  {'windwp/nvim-ts-autotag', config = true},
   {'nmac427/guess-indent.nvim', config = true},
   {'mg979/vim-visual-multi', branch = "master"}
 }
