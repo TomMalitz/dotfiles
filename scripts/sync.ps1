6622cd104e84a0489db3a8d056bbd4c66b0655ca
TomMalitz <tmalitz214@gmail.com>
2023-04-05 23:24:03 -0500

    adding ideavimrc file

--- scripts/sync.ps1
+++ scripts/sync.ps1
@@ -7,6 +7,7 @@ Write-Host "removing old config files..."
 Remove-Item "~/.config/nvim/" -Recurse -Force
 Remove-Item "~/.config/powershell/" -Recurse -Force
 Remove-Item "~/.config/wezterm/" -Recurse -Force
+Remove-Item "~/.ideavimrc" -Force
 if($isWindows) {
   Remove-Item "~/AppData/Local/nvim/" -Recurse -Force
 }
@@ -15,6 +16,7 @@ Write-Host "writing new config files..."
 Copy-Item "../nvim/" -Destination "~/.config/" -Recurse -Force
 Copy-Item "../powershell/" -Destination "~/.config/" -Recurse -Force
 Copy-Item "../wezterm/" -Destination "~/.config/" -Recurse -Force
+Copy-Item "../idea/.ideavimrc" -Destination "~/.ideavimrc" -Force
 if($isWindows) {
   Copy-Item "../nvim/" -Destination "~/AppData/Local/" -Recurse -Force
   Copy-Item "../powershell/profile.ps1" -Destination "~/Documents/PowerShell/Profile.ps1" -Force
