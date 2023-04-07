2dc3e14adebad605bcc29214fd9e5eabb202535f
TomMalitz <tmalitz214@gmail.com>
2023-04-07 13:05:32 -0500

    adding diagnostic keymap

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -94,6 +94,7 @@ return {
 --         vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
 --         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 --         vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+--         vim.keymap.set('n', 'gl', vim.diagnostic.open_float, bufopts)
 --       end
 --
 --       -- server configuration
