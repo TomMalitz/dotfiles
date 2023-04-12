b5fd16889c6f24078bbd784a2c6bc4eaf24c426c
TomMalitz <tmalitz214@gmail.com>
2023-04-11 20:27:09 -0500

    add border to lsp hover

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -130,8 +130,8 @@ return {
       }
 
       -- lsp float settings
-      -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-      -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
       -- diagnostics settings
       vim.diagnostic.config({
