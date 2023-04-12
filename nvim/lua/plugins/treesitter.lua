73a58fca9689925359fc3a0a6eeacf32c31dfc38
TomMalitz <tmalitz214@gmail.com>
2023-04-11 20:26:57 -0500

    add c_sharp to treesitter install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { "lua", "vim", "vue", "typescript", "tsx", "javascript", "java", "css", "cpp"},
+      ensure_installed = { "lua", "vim", "vue", "typescript", "tsx", "javascript", "java", "css", "cpp", "c_sharp"},
       highlight = { enable = true },
       indent = { enable = true }
     }
