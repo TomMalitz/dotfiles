8a6e79cd2b33b03755b7835a203eb921031c5d35
TomMalitz <tmalitz214@gmail.com>
2023-04-14 00:59:46 -0500

    overseer -^> lazy

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-lua/plenary.nvim',
   {'nvim-lualine/lualine.nvim', config = true},
-  {'stevearc/overseer.nvim', config = true},
-  'tpope/vim-fugitive'
+  {'stevearc/overseer.nvim', lazy = true, config = true},
+  {'tpope/vim-fugitive', lazy = true}
 }
