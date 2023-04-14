977c99a6f9219070cfa07f49c897321c8431b93c
TomMalitz <tmalitz214@gmail.com>
2023-04-14 00:59:33 -0500

    add leap config

--- nvim/lua/plugins/text-editing.lua
+++ nvim/lua/plugins/text-editing.lua
@@ -4,7 +4,13 @@ return {
   {'windwp/nvim-ts-autotag', config = true},
   {'nmac427/guess-indent.nvim', config = true},
   {'mg979/vim-visual-multi', branch = "master"},
+  {'ggandor/leap.nvim',
+    config = function()
+      require('leap').add_default_mappings()
+    end
+  },
   {"folke/zen-mode.nvim", 
+    lazy = true,
     keys = {
       {'<leader>z', '<cmd>ZenMode<cr>'}
     },
