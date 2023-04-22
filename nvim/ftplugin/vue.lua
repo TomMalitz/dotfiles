74a2b2d6b44e4e70543c5c95978698298064836d
TomMalitz <tmalitz214@gmail.com>
2023-04-22 01:16:59 -0500

    remove vue ftplugin file

--- nvim/ftplugin/vue.lua
+++
@@ -1,5 +0,0 @@
--- vue 3 volar take-over
-require'lspconfig'.volar.setup{
-  -- filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
-  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
-}
