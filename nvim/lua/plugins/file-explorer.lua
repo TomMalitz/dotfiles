c1e00ba95d2d290cc523c8ca724221102f776a87
TomMalitz <tmalitz214@gmail.com>
2023-04-22 01:17:08 -0500

    updating nvim tree api on_attach

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -28,35 +28,44 @@ return {
     {'<C-f>', '<cmd>NvimTreeToggle<cr>'},
   },
   config = function()
+    local function on_attach(bufnr)
+      local api = require('nvim-tree.api')
+
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+      vim.keymap.set('n', '<o>', api.node.open.edit, opts('Open'))
+      vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+
+      vim.keymap.set('n', 'D', api.fs.trash, opts('Trash'))
+      vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+      vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+      vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+      vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+      vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+      vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
+
+      vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+      vim.keymap.set('n', 'X', api.tree.collapse_all, opts('Collapse All'))
+
+      vim.keymap.set('n', 'F', api.live_filter.start, opts('Filter'))
+      vim.keymap.set('n', 'C', api.live_filter.clear, opts('Clear Filter'))
+
+      vim.keymap.set('n', 'm', api.marks.toggle, opts('Toggle Mark'))
+    end
+
     require("nvim-tree").setup({
+      on_attach = on_attach,
       sort_by = "case_sensitive",
       actions = {
         open_file = {
           resize_window = false
         }
       },
-      view = {
-        mappings = {
-          custom_only = true,
-          list = {
-            { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-            { key = "<Tab>", action = "preview" },
-            { key = "D", action = "trash" },
-            { key = "R", action = "refresh" },
-            { key = "X", action = "collapse_all" },
-            { key = "F", action = "live_filter" },
-            { key = "C", action = "clear_live_filter" },
-            { key = "a", action = "create" },
-            { key = "d", action = "remove" },
-            { key = "r", action = "rename" },
-            { key = "x", action = "cut" },
-            { key = "c", action = "copy" },
-            { key = "p", action = "paste" },
-            { key = "s", action = "system_open" },
-            { key = "m", action = "toggle_mark" },
-          },
-        },
-      }
     })
+
+
   end
 }
