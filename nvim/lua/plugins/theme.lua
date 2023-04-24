386b83f5192879309fe057a979f22a15c655de52
TomMalitz <tmalitz214@gmail.com>
2023-04-24 00:32:16 -0500

    update themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,49 +1,72 @@
-return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("tokyonight").setup({
-  --       style="night", -- storm, moon, night, day
-  --       -- transparent = true,
-  --       styles = {
-  --         -- sidebars = "transparent",
-  --         floats = "transparent"
-  --       },
-  --       on_colors = function(colors)
-  --         -- colors.bg_float = "#1b1d1e"
-  --         colors.bg_sidebar = "#1b1d1e"
-  --         colors.bg = "#2c2f30"
-  --         colors.fg_gutter = "#4a527c"
-  --       end
-  --     })
-  --     vim.cmd([[colorscheme tokyonight]]) 
-  --   end
-  -- }
-  -- {'posva/vim-vue'},
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        undercurl = true,
-        underline = true,
-        bold = true,
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = true,
-      })
-      vim.cmd([[colorscheme gruvbox]]) 
-    end
-  }
+local tokyonight = {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("tokyonight").setup({
+      style="night", -- storm, moon, night, day
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent"
+      },
+      on_colors = function(colors)
+        -- colors.bg_float = "#1b1d1e"
+        colors.bg_sidebar = "#1b1d1e"
+        colors.bg = "#2c2f30"
+        colors.fg_gutter = "#4a527c"
+      end
+    })
+    vim.cmd([[colorscheme tokyonight]]) 
+  end
 }
+
+local gruvbox = {
+  "ellisonleao/gruvbox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      undercurl = true,
+      underline = true,
+      bold = true,
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = true,
+    })
+    vim.cmd([[colorscheme gruvbox]]) 
+  end
+}
+
+local catppuccin = {
+  "catppuccin/nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('catppuccin').setup({
+      transparent_background = true
+    })
+    vim.cmd([[colorscheme catppuccin]]) 
+  end
+}
+
+local monokai = {
+  "patstockwell/vim-monokai-tasty",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd([[colorscheme vim-monokai-tasty]]) 
+  end
+} 
+
+local active_theme = gruvbox
+
+return active_theme
